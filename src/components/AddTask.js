91b0d055d68c832d8ac550ab4dd0ccdbf8c3d21b
Monika Mikrut <monikamikrut@outlook.com>
2020-11-30 20:03:02 +0100

    Updated AddTask component

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,13 +1,66 @@
 import React, { Component } from 'react';
+import './AddTask.css'
 
 class AddTask extends Component {
-    state = {  }
+
+    minDate = new Date().toISOString().slice(0,10);
+
+    state = { 
+        title:'',
+        description:'',
+        date: this.minDate,
+        group: 'personal',
+        important: false,
+     }
+
+     handleChange = (e) => {
+
+        const name = e.target.id;
+        const type = e.target.type;
+        console.log(name);
+        console.log(type);
+        if (type === 'text' || type === 'textarea' || type === 'select-one' || type === 'date'){
+            this.setState({
+                [name]:e.target.value
+            })
+        }else if (type === 'checkbox'){
+            this.setState({
+                [name]:e.target.checked
+            })
+        }
+     }
+
+     handleClick = (e) => {
+         e.preventDefault();
+       
+     }
+
     render() { 
+        
+        let maxDate = parseInt(new Date().toISOString().slice(0,4))+1;
+        maxDate = maxDate + '-12-31';
+
         return ( 
-            <div className="container">
-                <div>Dodaj task</div>
+            <div className="form">
+                 <h2>Nowe zadanie</h2>
+                 <form>
+                    <label htmlFor="title"> Tytuł zadania:</label>
+                    <input type="text" id="title" value={this.state.title} onChange={this.handleChange}></input>
+                    <label htmlFor="description"> Krótki opis:</label>
+                    <textarea id="description" value={this.state.description} onChange={this.handleChange}></textarea>
+                    <label htmlFor="date"> Do kiedy ma być wykonane:</label>
+                    <input type="date" id="date" value={this.state.date} min={this.minDate} max={maxDate} onChange={this.handleChange}></input>
+                    <label htmlFor="group">rodzaj zadania </label>
+                    <select value={this.state.type} id="group" onChange={this.handleChange}>
+                        <option value="personal">osobiste</option>
+                        <option value="work">zawodowe</option>
+                    </select>
+                    <label htmlFor="important"> Wazne 
+                    <input type="checkbox" checked={this.state.important} id="important" onChange={this.handleChange}/>
+                    </label>
+                    <button onClick={this.handleClick}>Dodaj do listy</button>
+                </form>
             </div>
-            
          );
     }
 }
