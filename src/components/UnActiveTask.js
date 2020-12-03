86a835310444afd8c1ad62f7a5fe8d760d17bcd6
Monika Mikrut <monikamikrut@outlook.com>
2020-12-03 13:48:53 +0100

    Added a delete button to UnActiveTask component

--- src/components/UnActiveTask.js
+++ src/components/UnActiveTask.js
@@ -3,13 +3,14 @@ import './UnActiveTask.css'
 
 const UnActiveTask = (props) => {
 
-    const {title, date, finishDate} = props.task;
+    const {id,title, date, finishDate} = props.task;
     const finishTime = new Date(finishDate).toLocaleString();
 
     return ( 
     <div className="unActiveTask">
-       <p><span>{date} - </span> {title}</p>
+       <p><span>{title}</span> ({date})</p>
        <p>Wykonano: <span>{finishTime}</span></p>
+       <button className="delete" onClick={() => props.delete(id)}>x</button>
     </div>
      );
 }
